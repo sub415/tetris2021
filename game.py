72c5233c6e45f4f69fff131b1f322d3a03d54da3
hhh <sub415@naver.com>
2021-03-30 16:41:14 +0900

    반복 추가 리스트 공부

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 print("게임을 시작합니다")	
 print("테트리스 시작")	
-print("1. 오른쪽 2. 왼쪽 3. 바꾸기")	
-number = input("숫자를 입력하세요: ")	
-print("당신이 입력한 숫자는?", number)	
-# 만약에 1번을 누르면 오른쪽으로 이동	
-if int(number) == 1:	
-    print("오른쪽으로 이동")	
-# 만약에 2번을 누르면 왼쪽으로 이동	
-if int(number) == 2:	
-    print("왼쪽으로 이동")	
-# 만약에 3번을 누르면 바꾸기 기능	
+###여기서부터 반복
+for i in range(10):
+    print("1. 오른쪽 2. 왼쪽 3. 바꾸기")	
+    number = input("숫자를 입력하세요: ")	
+    print("당신이 입력한 숫자는?", number)	
+    # 만약에 1번을 누르면 오른쪽으로 이동	
+    if int(number) == 1:	
+        print("오른쪽으로 이동")	
+    # 만약에 2번을 누르면 왼쪽으로 이동	
+    if int(number) == 2:	
+        print("왼쪽으로 이동")	
+    # 만약에 3번을 누르면 바꾸기 기능	
+    if int(number) == 3:
+        print("바꾸기")
+####반복 종료
---
+++ test.py
@@ -0,0 +1,14 @@
+for i in range(22):
+    print(i)
+# ㄹ 블록
+# ㄻㅇㄴ
+#ㄹㅇㄹ
+# ㄹㅇㄴㄹ
+# ㅇㄹㅇ
+block = ["ㄹ","ㅅ","ㅍ"]
+print(block)
+print("블록 갯수:", len(block))
+for i in range(3):
+    print(block[i])
+#for x in block:
+
